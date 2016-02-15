f3de45a6916a53355c9795588bdfad2beea7936b
ringill <ringill@mail.ru>
2016-02-15 23:43:53 +0300

    Added addon's files.

---
+++ mySellAll/localization.de.lua
@@ -0,0 +1,8 @@
+﻿local _, L = ...;
+if GetLocale() == "deDE" then
+ L["Sell "] = "Verkaufen ";
+ L["Repair "] = "Reparatur ";
+ L["Repair by guild"] = "Reparatur durch Gilde";
+ L["Too little money for repair "] = "zu wenig Geld fur Reparatur";
+ L["Result "] =  "Ergebnis ";
+end
---
+++ mySellAll/localization.ru.lua
@@ -0,0 +1,8 @@
+﻿local _, L = ...;
+if GetLocale() == "ruRU" then
+ L["Sell "] = "Продажа ";
+ L["Repair "] = "Чинимся за ";
+ L["Repair by guild"] = "Чинимся за счет гильдии";
+ L["Too little money for repair "] = "Мало денег на ремонт";
+ L["Result "] =  "Результат ";
+end
