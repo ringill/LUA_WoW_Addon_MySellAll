d6f071d313f4a009149711f89c9608e9dbeea638
Mikhail Guskov <ringill@users.noreply.github.com>
2018-06-23 00:11:16 +0300

    Prepare to release (v7.1.2226) (#1)
    * - add .gitignore
    - remove comments from .lua
    - add powershell build script
    
    * extend readme.md
    
    * add .rest file
    
    * fix deploy bugs:
    - need root folder in .zip
    - case sensitive .toc file

--- mySellAll/localization.de.lua
+++ mySellAll/localization.de.lua
@@ -1,8 +1,8 @@
-﻿local _, L = ...;
+﻿local _, L = ...
 if GetLocale() == "deDE" then
- L["Sell "] = "Verkaufen ";
- L["Repair "] = "Reparatur ";
- L["Repair by guild"] = "Reparatur durch Gilde";
- L["Too little money for repair "] = "zu wenig Geld fur Reparatur";
- L["Result "] =  "Ergebnis ";
+    L["Sell "] = "Verkaufen "
+    L["Repair "] = "Reparatur "
+    L["Repair by guild"] = "Reparatur durch Gilde"
+    L["Too little money for repair "] = "zu wenig Geld fur Reparatur"
+    L["Result "] = "Ergebnis "
 end
--- mySellAll/mysellall.lua
+++ mySellAll/mysellall.lua
@@ -1,4 +1,4 @@
-﻿local _, L = ...;
+﻿local _, L = ...
 local frame = CreateFrame("FRAME", "mySellAll")
 frame:RegisterEvent("MERCHANT_SHOW")
 frame:RegisterEvent("MERCHANT_CLOSED")
@@ -13,161 +13,93 @@ local endmoney
 local show
 
 local function ToMoney(Coppers)
-	 local FlMinus = false
-         if Coppers < 0 then FlMinus=true end
-         if FlMinus then Coppers = Coppers * (-1) end
-         local Gold = math.floor(Coppers / 10000)
-         local Silver = math.floor(Coppers / 100) % 100 
-         local Copper = Coppers % 100 
-         local ret = ""
-         if FlMinus then  ret = ((Gold ~= 0) and ("|cFFFF0000" .. Gold .. "g |r") or "") .. (((Silver ~= 0) or (Gold ~= 0)) and ("|cFFFF0000" .. Silver .. "s |r") or "") .. ("|cFFFF0000" .. Copper .. "c|r") 
-           else ret = ((Gold ~= 0) and ("|cffffd700" .. Gold .. "g |r") or "") .. (((Silver ~= 0) or (Gold ~= 0)) and ("|cffc7c7cf" .. Silver .. "s |r") or "") .. ("|cffeda55f" .. Copper .. "c|r") end
-         return ret
+	local FlMinus = false
+	if Coppers < 0 then
+		FlMinus = true
+	end
+	if FlMinus then
+		Coppers = Coppers * (-1)
+	end
+	local Gold = math.floor(Coppers / 10000)
+	local Silver = math.floor(Coppers / 100) % 100
+	local Copper = Coppers % 100
+	local ret = ""
+	if FlMinus then
+		ret =
+			((Gold ~= 0) and ("|cFFFF0000" .. Gold .. "g |r") or "") ..
+			(((Silver ~= 0) or (Gold ~= 0)) and ("|cFFFF0000" .. Silver .. "s |r") or "") .. ("|cFFFF0000" .. Copper .. "c|r")
+	else
+		ret =
+			((Gold ~= 0) and ("|cffffd700" .. Gold .. "g |r") or "") ..
+			(((Silver ~= 0) or (Gold ~= 0)) and ("|cffc7c7cf" .. Silver .. "s |r") or "") .. ("|cffeda55f" .. Copper .. "c|r")
+	end
+	return ret
 end
 
 local function eventHandler(self, event, ...)
-	--[[
-	local arg1 = ...
-	if event == "LOOT_BIND_CONFIRM" and GetNumPartyMembers() == 0 and GetNumRaidMembers() == 0 then
-		slot2loot[arg1] = true
-		print("BoPingEvent " .. arg1)
-		return
-	end
-	]]
-	
 	if event == "MERCHANT_SHOW" then
 		show = 0
 		beginmoney = GetMoney()
 		--sell all junk
 		for bag = 0, 4 do
-					if GetContainerNumSlots(bag) > 0 then
-						for slot = 1, GetContainerNumSlots(bag) do
-							local texture, itemCount, locked, quality = GetContainerItemInfo(bag, slot)
-							local link = GetContainerItemLink(bag, slot)
-							if link then
-								local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(link)
-								if itemRarity == 0 and itemSellPrice > 0 then
-									UseContainerItem(bag, slot)
-								end
-							end
+			if GetContainerNumSlots(bag) > 0 then
+				for slot = 1, GetContainerNumSlots(bag) do
+					local texture, itemCount, locked, quality = GetContainerItemInfo(bag, slot)
+					local link = GetContainerItemLink(bag, slot)
+					if link then
+						local itemName,
+							itemLink,
+							itemRarity,
+							itemLevel,
+							itemMinLevel,
+							itemType,
+							itemSubType,
+							itemStackCount,
+							itemEquipLoc,
+							itemTexture,
+							itemSellPrice = GetItemInfo(link)
+						if itemRarity == 0 and itemSellPrice > 0 then
+							UseContainerItem(bag, slot)
 						end
 					end
 				end
+			end
+		end
 		--repair
 		if (CanMerchantRepair()) then
-					--guild repair
-					if (CanGuildBankRepair()==1) then
-					RepairAllItems(1)
-					end
-					--self repair
-					RepairAllItems()
-		end
-	end
-	
-	if event == "MERCHANT_CLOSED" then
-	show = show+1
-		if (show==1) then
-			endmoney = GetMoney()
-			local raznica=(endmoney - beginmoney)
-			if raznica>0 then
-					print(L["Result "] .. "|TInterface\\Icons\\INV_Misc_Coin_01:16|t " .. ToMoney(raznica))
-			end
-			if raznica<0 then
-					print(L["Result "] .. "|cFFFF0000 -|r " .. ToMoney(raznica))
-			end
-		end
-	end
-
-	--[[
-	local goflag
-    local countsell=0
-	local cost=0
-	if (not busy) and (not IsShiftKeyDown()) then
-		busy = true
-		if event == "MERCHANT_SHOW" then
-			for bag = 0, 4 do
-				if GetContainerNumSlots(bag) > 0 then
-					for slot = 1, GetContainerNumSlots(bag) do
-						local texture, itemCount, locked, quality = GetContainerItemInfo(bag, slot)
-						local link = GetContainerItemLink(bag, slot)
-						if link then
-							local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(link)
-							if itemRarity == 0 and itemSellPrice > 0 then
-								UseContainerItem(bag, slot)
-								-- print("[" .. itemName .. "]х" .. itemCount .. " за " .. ToMoney(itemSellPrice*itemCount))
-								countsell = countsell + (itemSellPrice*itemCount)
-							end
-						end
-					end
-				end
-			end
-			if (countsell~=0) then
-			print(L["Sell "] .. ToMoney(countsell))
-			end
-
-			if CanMerchantRepair() then
-				cost = GetRepairAllCost()
-				if (CanGuildBankRepair()==1) then
-				--print("Guild repair aviable")
+			--guild repair
+			if (CanGuildBankRepair() == 1) then
 				RepairAllItems(1)
-				end
-				if cost > 0 then
-					local money = GetMoney()
-					if money > cost then
-						RepairAllItems()
-						if (cost~=0) then
-						print(L["Repair "] .. ToMoney(cost))
-						end
-					else
-						if (cost~=0) then
-						print(L["Too little money for repair "])
-						end
-					end
-				end
-		
 			end
-			if (countsell~=0 or cost~=0) then
-				local raznica=(countsell - cost)
-					if raznica>0 then
-					print(L["Result "] .. "|TInterface\\Icons\\INV_Misc_Coin_01:16|t " .. ToMoney(raznica))
-					end
-					if raznica<0 then
-					print(L["Result "] .. "|cFFFF0000 -|r " .. ToMoney(raznica))
-					end
-			end
+			--self repair
+			RepairAllItems()
 		end
-		busy = false
-	end
-	]]
-	--return
-end
-
---[[
-local function DoTheStuff()
-
-	if UIParent:IsEventRegistered("LOOT_BIND_CONFIRM") then
-		UIParent:UnregisterEvent("LOOT_BIND_CONFIRM")
 	end
 
-		for i in pairs(slot2loot) do
-			ConfirmLootSlot(i)
-			print("Confirm " .. i)
-			LootSlot(i+1)
-			slot2loot[i] = nil
+	if event == "MERCHANT_CLOSED" then
+		show = show + 1
+		if (show == 1) then
+			endmoney = GetMoney()
+			local raznica = (endmoney - beginmoney)
+			if raznica > 0 then
+				print(L["Result "] .. "|TInterface\\Icons\\INV_Misc_Coin_01:16|t " .. ToMoney(raznica))
+			end
+			if raznica < 0 then
+				print(L["Result "] .. "|cFFFF0000 -|r " .. ToMoney(raznica))
+			end
 		end
+	end
 end
-]]
 
 frame:SetScript("OnEvent", eventHandler)
 frame:SetScript("OnUpdate", DoTheStuff)
 
-OptPanel = CreateFrame( "Frame", "mySellAllOptions", UIParent );
-OptPanel.name = "mySellAll";
-local AAA = CreateFrame('CheckButton', "Sencored", OptPanel, 'InterfaceOptionsCheckButtonTemplate')
-AAA:SetPoint('TOPLEFT', 10, -72)
+OptPanel = CreateFrame("Frame", "mySellAllOptions", UIParent)
+OptPanel.name = "mySellAll"
+local AAA = CreateFrame("CheckButton", "Sencored", OptPanel, "InterfaceOptionsCheckButtonTemplate")
+AAA:SetPoint("TOPLEFT", 10, -72)
 
-
-InterfaceOptions_AddCategory(OptPanel);
+InterfaceOptions_AddCategory(OptPanel)
 
 SLASH_MYSELLALL1 = "/msa"
 SlashCmdList["MYSELLALL"] = function()
